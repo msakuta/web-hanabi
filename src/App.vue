97d9fd2039891fdf95c493b0630190dfdbe4dfd5
msakuta <masahiro.sakuta@gmail.com>
2021-03-02 04:09:37 +0900

    Initial commit with Vuejs

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="appContainer">
+    <h1>Web-hanabi</h1>
+    <web-brokus/>
+    <hr>
+    Powered by <a href="https://v3.vuejs.org/">Vue.js</a><br>
+    Source on <a href="https://github.com/msakuta/web-hanabi">GitHub</a>.
+  </div>
+</template>
+
+<script>
+import WebBrokus from './components/WebHanabi.vue'
+
+export default {
+  name: 'App',
+  components: {
+    WebBrokus
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 30px;
+  margin-bottom: 30px;
+}
+
+.appContainer {
+  width: 650px;
+  position: relative;
+  margin: auto;
+}
+</style>
---
+++ src/components/WebHanabi.vue
@@ -0,0 +1,76 @@
+<template>
+  <h1>Hello</h1>
+</template>
+
+<script>
+export default {
+  name: 'WebHanabi',
+
+  setup(){
+    return {
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.outerFrame {
+  position: relative;
+  left: 320px;
+  margin-left: -320px;
+  right: 0px;
+  width: 640px;
+  height: 640px;
+  border: solid 1px red;
+  background-color: white;
+}
+.playerFrame {
+  border: 1px solid black;
+  position: relative;
+  width: 640px;
+}
+.inactive {
+  background-color: #afafaf;
+}
+.cell {
+  border: solid 1px black;
+  width: 32px;
+  height: 32px;
+}
+.ridge {
+  border: ridge 4px;
+  border-radius: 4px;
+  width: 24px;
+  height: 24px;
+}
+.buttonIcon {
+  border: outset 4px;
+  border-radius: 4px;
+  width: 48px;
+  height: 48px;
+}
+.cellInternalFrame {
+  border: solid 2px #ff00ff;
+  position: absolute;
+  left: 2px;
+  top: 2px;
+  width: 18px;
+  height: 18px;
+  pointer-events: none;
+}
+</style>
